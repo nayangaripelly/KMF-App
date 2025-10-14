e4fd04ef145d1971f8bfefb671aa9a081f920823
Sai Nayan <sainayan@Mac.mshome.net>
2025-10-15 00:34:45 +0530

    implemented signup and signin endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
 const app = express();
 
+app.use(express.json());
+app.use("/api/v1/users",userRouter);
+
+dotenv.config();
+const mongourl = process.env.MONGODB_URL as string;
+async function main()
+{
+    await mongoose.connect(mongourl);
+    app.listen(3000);
+}
+
+main();
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,76 @@
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import { userModel } from "../models/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const userRouter = Router();
+
+const jwtsecret = process.env.JWT_SECRET as string;
+userRouter.post("/signup",async function(req, res)
+{
+    const {username, emailId, password, role} = req.body;
+    const hash = await bcrypt.hash(password,5);
+    try
+    {
+        await userModel.create({
+            username,
+            emailId,
+            passwordhash:hash,
+            role,
+            createdAt:Date.now()
+        });
+        res.status(201).json({
+            msg:"user successfully created!!"
+        })
+    }catch(e)
+    {
+        res.status(409).json({
+            msg:"account with email already exists or something went wrong try again"
+        })
+    }
+});
+
+userRouter.post("/signin",async function(req,res)
+{
+    console.log(req.body);
+    const {emailId, password} = req.body;
+    try
+    {
+        const user = await userModel.findOne({emailId});
+        if(!user)
+        {
+            res.status(404).json({
+                msg:"user doesn't exist, incorrect emailId",
+            });
+            return;
+        }
+        console.log("after user found", user);
+        const hash = user?.passwordhash as string;
+        const match = await bcrypt.compare(password,hash);
+        if(!match)
+        {
+            res.status(401).json({
+                msg:"incorrect credentials"
+            });
+            return;
+        }
+        console.log("after password checked");
+        console.log(jwtsecret);
+        const token = jwt.sign({
+            id:user?._id
+        },jwtsecret);
+        
+        console.log("after signing token",token);
+        res.status(200).json({
+            token
+        });
+    }catch(e)
+    {
+        res.json({
+            msg:"something went wrong try again"
+        })
+    }
+});
+
+export default userRouter;
